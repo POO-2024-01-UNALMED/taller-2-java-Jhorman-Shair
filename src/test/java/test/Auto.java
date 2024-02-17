d66b9d54f37e39c891ef5d31f06f8e7328ccc143
Jhorman-Shair <jhramirezhe@unal.edu.co>
2024-02-17 13:08:11 -0500

    Odio el bot de autograding
    nuevo intento

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -3,37 +3,40 @@ package test;
 public class Auto {
 	String modelo;
 	int precio;
-	Asiento[] asientos;
+    Asiento[] asientos ;
 	String marca;
 	Motor motor;
 	int registro;
+	int n_asientos;
 	static int cantidadCreados;
-	
-	int cantidadAsientos(){
-		int x=0;
-		for (int i=0;i<asientos.length;i++) {
-			if (asientos[i]!=null){
-				x+=1;
+  	
+	 	int cantidadAsientos() { 
+		for(int i=0;i< asientos.length;i++) {
+			if (asientos[i]!= null) {	
+				n_asientos+=1;
 			}
-			return x;
-	     }
-	}
-	String verificarIntegridad(){
-		String resultado;
-		if(registro==motor.registro) {
-			resultado="Auto original";
-		}else{	
-			resultado="Las piezas no son originales";
+    		  		
 		}
-		if (resultado=="Auto original") {
-			for (int i=0;i<asientos.length;i++) {
-				if (asientos[i]!=null){
-					if (asientos[i].registro!=registro) {
-						resultado="Las piezas no son originales";
-						break;
-					}	
-		}
+		return n_asientos;
 	}
-		return resultado;
-  }
+	
+	 	String verificarIntegridad(){
+		String resultado="Auto original";
+		if (this.registro== motor.registro ) {
+			for(int i=0;i< asientos.length;i++) {
+				if (asientos[i]!= null) {	
+				 if(asientos[i].registro!=this.registro) {
+					 resultado="Las piezas no son originales";
+					 break;
+				 }
+				}
+				
+		}
+		}
+		else {
+			 resultado="Las piezas no son originales";
+		}
+		return resultado;	
+	}
+	
 }
